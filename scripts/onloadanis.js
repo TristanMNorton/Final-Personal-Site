78ae4b58720a2c60e7b1d3b3af3e2aad8986635c
Tristan Norton <tristan.m.norton@gmail.com>
2017-07-06 21:56:01 -0400

    Completely reconfigured bio/skills section with a whole bunch of flexbox trickery and acrobatics. Took way more time than I intended as I want to start work on the portfolio secion. Although I'm still not quite clear as whether I should keep the portfolio compact and focusing on my best work or show a wider breadth of my design history. I'll deliberate until I sit down and peck away at this shit again.

--- scripts/onloadanis.js
+++ scripts/onloadanis.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
     var globalWidth = $(window).width();
+
     //Makes header slide in all nice and stuff
     if (globalWidth >= 740) {
         $('#headerTitle').css('display', 'none');
         $('#headerTitle').delay( 800 ).fadeIn(1000);
-    }
-    
-    
+      }
 });
