e6a6afcfdbb358e0073629e2a2fd53d90464d055
Tristan Norton <tristan.m.norton@gmail.com>
2017-08-05 17:37:27 -0400

    cleaned up some clutter

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -9,14 +9,14 @@ console.log(imgThumb);
 
 //Loops through thumbnails and adds event listeners 
 for(i = 0; i < 8; i++) {
-    imgThumb[i].addEventListener('click', function(){
-        console.log(this.children[0].currentSrc);
+    var currentItem = imgThumb[i];
+    $(currentItem).on("click touchstart", function(){
         displaySample(this);
     });
 };
 
 //changes Title and Description in item display based on which is clicked
-//Makes display visible
+//Makes display visibler
 function displaySample(img) {
     var bigImg = (img.children[0].currentSrc + 'big');
     var curCompTitle = (img.children[0].title);
@@ -28,7 +28,7 @@ function displaySample(img) {
 };
 
 //Closes display
-closeButton.addEventListener('click', function() {
+closeButton.addEventListener(touchEvent, function() {
     portItem.style.visibility = 'hidden';
     portItem.style.opacity = '0';
 });
