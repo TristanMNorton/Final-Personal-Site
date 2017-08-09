500aeb455ed7c5fae1972aae8cf0083e44a45238
Tristan Norton <tristan.m.norton@gmail.com>
2017-08-08 23:48:36 -0400

    Almost done. Refined the design. Just need to finish up the social media portion and it's ready to go live tomorrow night.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -3,6 +3,7 @@ var portItem = document.getElementById('portFullDisplayWrapper');
 var closeButton = document.getElementById('closeButton');
 var companyTitle = document.getElementById('companyTitle');
 var companyDescription = document.getElementById('companyDescription');
+var portFullItem = document.getElementById('portFullItem');
 var imgThumb = document.getElementsByClassName('portItem');
 
 console.log(imgThumb);
@@ -16,19 +17,20 @@ for(i = 0; i < 8; i++) {
 };
 
 //changes Title and Description in item display based on which is clicked
-//Makes display visibler
+//Makes display visible
 function displaySample(img) {
-    var bigImg = (img.children[0].currentSrc + 'big');
+    var imgSubString = img.children[0].currentSrc.substring(0, img.children[0].currentSrc.indexOf('.')); 
     var curCompTitle = (img.children[0].title);
     var curCompAlt = (img.children[0].alt);
     companyTitle.innerHTML = curCompTitle;
     companyDescription.innerHTML = curCompAlt;
     portItem.style.visibility = 'visible';
     portItem.style.opacity = '1';
+    portFullItem.src = imgSubString + '.jpg';
 };
 
 //Closes display
-closeButton.addEventListener(touchEvent, function() {
+closeButton.addEventListener('click', function() {
     portItem.style.visibility = 'hidden';
     portItem.style.opacity = '0';
 });
