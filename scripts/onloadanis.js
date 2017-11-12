81a890d3bcefd07d751bca55c30d2e12e7a77c81
Tristan Norton <tristan.m.norton@gmail.com>
2017-11-12 12:41:29 -0500

    checking this shiznit

--- scripts/onloadanis.js
+++ scripts/onloadanis.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     var globalWidth = $(window).width();
 
-    //Makes header slide in all nice and stuff
+    //Makes header fade in all nice and stuff
     if (globalWidth >= 740) {
         $('#headerTitle').css('display', 'none');
         $('#headerTitle').delay( 800 ).fadeIn(1000);
       }
+
+    //
 });
